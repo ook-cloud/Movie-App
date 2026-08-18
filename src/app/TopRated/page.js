be91e2a31fe404cd70b80639a10c522241b34d0b
ook-cloud <ooktb57@gmail.com>
2026-08-18 09:16:17 +0800

    mday

---
+++ src/app/TopRated/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function TopRatedPage() {
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center overflow-x-hidden">
+      hi
+    </div>
+  );
+}
